99ded2b4547ef20805781cf077bbfc1ca5bf166f
fnand <fwschenck@gmail.com>
2021-12-06 10:45:03 +0100

    Add day 4 solution

---
+++ day_4/README.md
@@ -0,0 +1,10 @@
+## Things I learned:
+
+
+* I tried the CSV package instead of DelimitedFiles as I could choose the amount of lines to read easily 
+
+* DataFrames in Julia similar to pandas
+
+* Had to mess around with reshape and permutedims a bit to get array in right order. Need to figure out why values ended up in different order than I naively expected. 
+
+* `replace` is a super useful function
--- day_4/day_4.jl
+++ day_4/day_4.jl
@@ -2,14 +2,15 @@ using CSV
 using DataFrames
 
 in_file = "input_4.txt"
-nums = Matrix(CSV.read(in_file, DataFrame, delim = ',', limit = 1, header = false))
+in_nums = Matrix(CSV.read(in_file, DataFrame, delim = ',', limit = 1, header = false))
 
 
 in_tables = Matrix(CSV.read(in_file, DataFrame, skipto = 2, delim = " ", header = false, ignorerepeated= true))
 in_tables = permutedims(reshape(in_tables, (5, 100, 5)), [2,1,3]);
 
-
-function find_winner(tables)
+# Changes numbers to -1 if they have been called
+# When a board wins it will have a column/row that sums to -5
+function find_winner(tables, nums)
     for n in nums
         tables = replace(tables, n=>-1)
     
@@ -27,4 +28,33 @@ function find_winner(tables)
 end
 
 
-println("The solution to part one is: ", find_winner(in_tables))
+# I tried being clever and search from the back, but ended having to subtract 1 in many places, which gets a bit ugly. 
+function find_last_winner(tables, nums)
+
+    tables = -1*tables .-1 # -1 because I use the fact that numbers are pos/neg to find bingo, and 0 is neither 
+
+    nums = reverse(-1*nums .-1) # Search from the back and correct for -1 
+
+    for (i, n) in enumerate(nums)
+        tables = replace(tables, n=>-1*n)
+
+	# Basically just ask whether all boards win
+        # As we start at the back all boards are winners and we find the point where one stops winning. 
+        condition = all(sum(sign.(tables), dims = 2) .!= -5, dims = 3) .&& all(sum(sign.(tables), dims = 3) .!= -5, dims = 2)
+        
+        if any(condition)
+            ind = findall(condition)[1][1]
+	    # ugly corrections to make up for using negative numbers
+            tab = replace(tables[ind,:,:], -1*n=> n)
+            tab = replace(x -> sign(x) == -1 ? 0 : x, tab.-1) 
+            return sum(tab)*(-1*n - 1)
+        end
+
+    end
+end
+
+
+
+
+println("The solution to part one is: ", find_winner(in_tables, in_nums))
+println("The solution to part two is: ", find_last_winner(in_tables, in_nums))
